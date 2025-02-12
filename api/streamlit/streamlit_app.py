6494c94c9d570b1e99bac76428d84b3e263f4fc7
Elvin Ellis Smith Jr <svanati@users.noreply.github.com>
2025-02-12 15:09:29 -0500

    Update streamlit_app.py

--- api/streamlit/streamlit_app.py
+++ api/streamlit/streamlit_app.py
@@ -15,5 +15,9 @@ name_on_order = st.text_input("Name on Smoothie:")
 if name_on_order:
     st.write("The name on your Smoothie will be:", name_on_order)
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+if ingredients_list:
+        ingredients_list = ''
+
+        for fruit_chosen in ingrediants_list:
+                smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+                sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
